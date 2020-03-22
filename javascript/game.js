e7ed8e2bf1171f72602172babf8f88313333c1dc
Tyler Blakeman <tylerblakeman@gmail.com>
2020-03-22 08:24:36 -0500

    added jquery functions for button presses on the images, added images to html, added CSS to html, changed layout of HTML and wrote functions to update current score when buttons are pressed

--- javascript/game.js
+++ javascript/game.js
@@ -5,39 +5,28 @@ var emerald;
 var numberToMatch;
 var wins;
 var losses;
-var totalScore;
-
-
-
-
-
-
-
-
+var totalScore = 0;
 
 
 getRandomNumber();
 
 
-
-
-
-
-
 //function to generate the random number to be matched between 50 and 100
 function getRandomNumber(){
     //generating a random number 19 - 120
-    var numberToMatch = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    numberToMatch = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     //generating random numbers between 1 and 12 for each of the gemstones
-    var ruby = Math.floor(Math.random() * (12-1 + 1)) + 1;
-    var diamond = Math.floor(Math.random() * (12-1 + 1)) + 1;
-    var topaz = Math.floor(Math.random() * (12-1 + 1)) + 1;
-    var emerald = Math.floor(Math.random() * (12-1 + 1)) + 1;
+    ruby = Math.floor(Math.random() * (12-1 + 1)) + 1;
+    diamond = Math.floor(Math.random() * (12-1 + 1)) + 1;
+    topaz = Math.floor(Math.random() * (12-1 + 1)) + 1;
+    emerald = Math.floor(Math.random() * (12-1 + 1)) + 1;
     console.log("this is the random number you're looking for", numberToMatch)
     console.log("ruby: ", ruby);
     console.log("diamond: ", diamond);
     console.log("topaz: ", topaz);
     console.log("emerald: ", emerald)
+    //outputting the score to match to html
+    $(".scoreToMatch").html(numberToMatch)
 }
     
 function checkScore(){
@@ -53,13 +42,35 @@ function checkScore(){
     }
 }
 
-function displayReset(){
-    alert("You win")
-    reset()
-}
-
 function reset(){
     totalScore = 0;
     getRandomNumber();
-
 }
+
+$(".ruby").click(function(){
+    totalScore = totalScore + ruby;
+    console.log("ruby")
+    console.log(ruby)
+    $(".userScore").html(totalScore)
+});
+
+$(".diamond").click(function(){
+    totalScore = totalScore + diamond;
+    console.log("diamond")
+    console.log(diamond)
+    $(".userScore").html(totalScore)
+});
+
+$(".topaz").click(function(){
+    totalScore = totalScore + topaz;
+    console.log("topaz")
+    console.log(topaz)
+    $(".userScore").html(totalScore)
+});
+
+$(".emerald").click(function(){
+    totalScore = totalScore + emerald;
+    console.log("emerald")
+    console.log(emerald)
+    $(".userScore").html(totalScore)
+});
